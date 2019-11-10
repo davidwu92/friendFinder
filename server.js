1603ad56a6b6547c0aad8ff913abfce945ebe963
David Wu <davidjwu92@gmail.com>
2019-11-10 15:25:08 -0800

    Application is functioning

--- server.js
+++ server.js
@@ -7,15 +7,23 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 let friends = [
-  {name:"Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores:[5,1,4,4,5,1,2,5,4,1]
+  {name:"Samurai Jack",
+  photo: "http://media.comicbook.com/2017/10/36190303uan-samuraijack-1449189177030-1280w-skmh-1038393.jpg",
+  scores: [3,3,3,3,3,3,3,3,3,3]
+  },
+  {name: "SpongeBob Squarepants",
+  photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/330px-SpongeBob_SquarePants_character.svg.png",
+  scores: [1,1,1,1,1,1,1,1,1,1]
+  },
+  {name: "Princess Zelda",
+  photo: "https://gamepedia.cursecdn.com/zelda_gamepedia_en/thumb/e/eb/ALBW_Princess_Zelda_Artwork.png/260px-ALBW_Princess_Zelda_Artwork.png?version=4a09897be3da1ddb1f9614ef5a33f2dc",
+  scores: [5,5,5,5,5,5,5,5,5,5]
   }
 ]
 
 app.post('/newFriend', (req, res)=>{
-  console.log(req.body)
   friends.push(req.body)
+  res.sendStatus(200)
 })
 
 app.get('/getFriends', (req, res)=>{
